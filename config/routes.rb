5f8a97b921188bb230737e3beb1639ee41c8ef84
ChulMin Kim <kbs4674@likelion.org>
2018-09-05 16:45:11 +0000

    devise 로그아웃 먹통문제 해결중

--- config/routes.rb
+++ config/routes.rb
@@ -37,8 +37,6 @@ Rails.application.routes.draw do
   # 알림
   resources :new_notifications
   
-  match 'users/sign_out' => "devise/sessions#destroy"
-  
   devise_for :users
   
   resources :conversations, only: [:create] do
